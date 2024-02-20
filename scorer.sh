b932df2c45bd56038b6186833277869eaf118bae
volbem <31348308+volbem@users.noreply.github.com>
2024-02-20 14:35:04 +0800

    Delete scorer.sh

--- scorer.sh
+++
@@ -1,8 +0,0 @@
-# nohup python scorer.py --test_file results_original/output.json --output_dir results_original/ > eval_original.log &
-# nohup python scorer.py --test_file results_cot/output_clear.json --output_dir results_cot/ > eval_cot.log &
-# nohup python scorer.py --test_file results_action/output_clear.json --output_dir results_action/ > eval_action.log &
-# nohup python scorer.py --test_file results_cot_split/output_clear.json --output_dir results_cot_split/ --is_action True > eval_cot_split.log &
-python scorer.py \
-    --test_file ../results_cot_split/output_clear.json \
-    --output_dir ../results_cot_cosine \
-    --is_action True
