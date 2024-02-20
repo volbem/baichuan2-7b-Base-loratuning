e9cec5cbe1111af19fac2f44e207226c5a38a4fc
volbem <31348308+volbem@users.noreply.github.com>
2024-02-20 15:05:01 +0800

    Update and rename test_multigpu.sh to test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
+#单卡测试
+#CUDA_VISIBLE_DEVICES=0 python test.py \
+#    --model_path ../baichuan2/Baichuan2-7B-Base \
+#    --data_path ./data/kst_test_cot.json \
+#    --out_file ./results_cot_split/output_singlegpu.json \
+#    --batch_size 4 \
+#    --max_new_tokens 128 \
+#
+#多卡测试
 #accelerate launch \
 #    --num_processes 2\
 #    --config_file sft.yaml\
