9aa5e576521453bf303563bf89fc0a88ce8b5acf
volbem <31348308+volbem@users.noreply.github.com>
2024-02-20 15:05:09 +0800

    Delete test_singlegpu.sh

--- test_singlegpu.sh
+++
@@ -1,9 +0,0 @@
-
-CUDA_VISIBLE_DEVICES=0 python test.py \
-    --model_path ../baichuan2/Baichuan2-7B-Base \
-    --data_path ./data/kst_test_cot.json \
-    --out_file ./results_cot_split/output_singlegpu.json \
-    --batch_size 4 \
-    --max_new_tokens 128 \
-
-    
